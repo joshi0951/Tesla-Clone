291477ff9b1aa911dba446f56cb92fa841bfc6d8
Harsh <joshi0951@gmail.com>
2021-01-03 11:20:27 +0530

    commit

--- components/CarsList/cars.js
+++ components/CarsList/cars.js
@@ -3,17 +3,17 @@ export default [{
   tagline: 'Starting at $69,420',
   image: require('../../assets/images/ModelS.jpeg'),
 }, {
-  name: 'Model 3',
+  name: 'Model 3 ',
   tagline: 'Order Online for',
   taglineCTA: 'Touchless Delivery',
   image: require('../../assets/images/Model3.jpeg'),
 }, {
-  name: 'Model X',
+  name: 'Model X ',
   tagline: 'Order Online for',
   taglineCTA: 'Touchless Delivery',
   image: require('../../assets/images/ModelX.jpeg'),
 }, {
-  name: 'Model Y',
+  name: 'Model Y ',
   tagline: 'Order Online for',
   taglineCTA: 'Touchless Delivery',
   image: require('../../assets/images/ModelY.jpeg'),
